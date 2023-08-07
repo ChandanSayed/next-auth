c6c91c412ee5dd361ba07160ec41814d7d58c83f
unknown <hangzhou635@gmail.com>
2023-08-07 16:06:49 +0600

    User Registration added

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,21 +1,45 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
 
 export default function SignUp() {
+  const router = useRouter();
   const [user, setUser] = useState({
     email: '',
     password: '',
-    name: ''
+    username: ''
   });
+  const [buttonDisable, setButtonDisable] = useState(true);
+
+  useEffect(() => {
+    if (user.username.length > 0 && user.email.length > 0 && user.password.length > 0) {
+      setButtonDisable(false);
+    }
+    return () => {
+      setButtonDisable(true);
+    };
+  }, [user]);
+
+  async function createUser(e: FormEvent) {
+    e.preventDefault();
+    try {
+      const res = await axios.post('/api/users/signup', user);
+      console.log('Signup Success', res.data);
+      router.push('/login');
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
 
   return (
     <div className="flex flex-col min-h-screen bg-black items-center justify-center text-white">
       <h2>Sign Up</h2>
-      <form action="#">
+      <form action="#" className="text-black" onSubmit={createUser}>
         <div className="input-control">
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" onChange={e => setUser({ ...user, name: e.target.value })} />
+          <input type="text" id="name" name="name" onChange={e => setUser({ ...user, username: e.target.value })} />
         </div>
         <div className="input-control">
           <label htmlFor="email">Email</label>
@@ -25,7 +49,9 @@ export default function SignUp() {
           <label htmlFor="password">Password</label>
           <input type="password" id="password" name="password" onChange={e => setUser({ ...user, password: e.target.value })} />
         </div>
-        <button className="btn border border-gray-700">Sign Up</button>
+        <button disabled={buttonDisable ? true : false} className={`btn border text-white border-gray-700`}>
+          Sign Up
+        </button>
       </form>
       <Link href={`/login`}>Login</Link>
     </div>
